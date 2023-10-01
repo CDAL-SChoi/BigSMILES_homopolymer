cd571b1e436111f5ad00e02ac433324fa7389220
Sunho Choi <50295574+CDAL-SChoi@users.noreply.github.com>
2023-10-02 00:13:50 +0900

    Update test_s2bigs_withTg.py

--- test/test_s2bigs_withTg.py
+++ test/test_s2bigs_withTg.py
@@ -6,9 +6,9 @@ start = time.time()
 k = SMILES2BigSMILES()
 
 #k.Data_Load_csv('with_Tg/Bicerano_bigsmiles.csv', SMILES='SMILES', encoding='cp949')
-#k.s2bigs_converting('with_Tg/Bicerano_')
+#k.Converting('with_Tg/Bicerano_')
 
 k.Data_Load_csv('with_Tg/JCIM_sup_bigsmiles.csv', SMILES='SMILES', encoding='cp949')
-k.s2bigs_converting('with_Tg/JCIM_', move_parallel=0)
+k.Converting('with_Tg/JCIM_', move_parallel=0)
 
 print(time.time()-start)
