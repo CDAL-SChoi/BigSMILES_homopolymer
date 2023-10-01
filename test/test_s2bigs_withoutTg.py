7b1daae09d8d0b2cf523602b77fa35d18cb0b836
Sunho Choi <50295574+CDAL-SChoi@users.noreply.github.com>
2023-10-02 00:14:16 +0900

    Update test_s2bigs_withoutTg.py

--- test/test_s2bigs_withoutTg.py
+++ test/test_s2bigs_withoutTg.py
@@ -8,6 +8,6 @@ k = SMILES2BigSMILES()
 k.Data_Load_txt('dataset/generated_polymer_smiles_train.txt', N=10000000)
 ### This data is accessible at https://zenodo.org/record/7766806 (last checked 23.09.30)
 
-k.s2bigs_converting('result_0925/switch_len85_', move_parallel=0)
+k.Converting('polyBERT_len85_', move_parallel=0)
 
 print(time.time()-start)
