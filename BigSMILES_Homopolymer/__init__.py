fc931d9477b8fc832b9f56aaa49908f2165dd8f1
Sunho Choi <50295574+CDAL-SChoi@users.noreply.github.com>
2023-10-01 14:27:37 +0900

    Add files via upload

---
+++ BigSMILES_Homopolymer/__init__.py
@@ -0,0 +1,1 @@
+from .BigSMILES_Homopolymer import *
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BigSMILES_Homopolymer',
+    version='1.0.0',
+    description='An automated BigSMILES conversion workflow for homopolymeric macromolecules',
+    url='https://github.com/CDAL-SChoi/BigSMILES_homopolymer',
+    author='Sunho Choi',
+    author_email='schoi_@korea.ac.kr',
+    license='schoi',
+    packages=find_packages(),
+    install_requires=['numpy', 'openpyxl==3.0.9', 'pandas', 'rdkit==2022.9.5', 'regex==2022.6.2'],
+)
